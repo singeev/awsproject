7f5798bcae6d5d0bf78942f05c10b037d3ff2b6c
singeev <singeev@loyaltyplant.com>
2017-11-20 17:57:45 +0800

    Init spring boot project in IDEA

---
+++ src/main/java/com/awsproject/AwsprojectApplication.java
@@ -0,0 +1,12 @@
+package com.awsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsprojectApplication.class, args);
+	}
+}
