1100e1231ed98f0ed47d531e627bd8cdb4dabd33
singeev <singeev@loyaltyplant.com>
2017-11-22 17:55:49 +0800

    Externalized webmaster login and email details

--- src/main/java/com/awsproject/AwsprojectApplication.java
+++ src/main/java/com/awsproject/AwsprojectApplication.java
@@ -9,6 +9,7 @@ import com.awsproject.enums.RolesEnum;
 import com.awsproject.utils.UserUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,6 +24,15 @@ public class AwsprojectApplication implements CommandLineRunner{
 	@Autowired
 	private UserService userService;
 
+	@Value("${webmaster.username")
+	private String webmasterUserName;
+
+	@Value("${webmaster.password")
+	private String webmasterPassword;
+
+	@Value("${webmaster.email")
+	private String webmasterEmail;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AwsprojectApplication.class, args);
 	}
@@ -30,12 +40,10 @@ public class AwsprojectApplication implements CommandLineRunner{
 	@Override
 	public void run(String... strings) throws Exception {
 
-		String userName = "Hulk";
-		String email = "green@gmail.com";
-
-		User user = UserUtils.createBasicUser(userName, email);
+		User user = UserUtils.createBasicUser(webmasterUserName, webmasterEmail);
+		user.setPassword(webmasterPassword);
 		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(new UserRole(user, new Role(RolesEnum.BASIC)));
+		userRoles.add(new UserRole(user, new Role(RolesEnum.ADMIN)));
 		LOGGER.debug("Creating user with username {}", user.getUsername());
 		userService.crateUser(user, PlansEnum.PRO, userRoles);
 		LOGGER.debug("User {} created", user.getUsername());
